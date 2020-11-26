ebbb2774eda4532c6e73d3c6e49db2646fd60f62
Lauracalderon25 <lauecalderon@gmail.com>
2020-11-25 19:50:26 -0600

    Avance 2 se creo csv

--- src/com/lauracalderon/AgendaCitas.java
+++ src/com/lauracalderon/AgendaCitas.java
@@ -1,8 +1,9 @@
 package com.lauracalderon;
 
-import java.io.File;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 
 public class AgendaCitas {
@@ -15,17 +16,58 @@ public class AgendaCitas {
     public static String HORA;
     public static String DESCRIPCION;
 
-    //String separator = FileSystems.getDefault().getSeparator();
-    //String filename = String.format("src%scom%slauracalderon%sArchivoDB.csv",separator,separator,separator,separator,separator,separator);
-    static File ArchivoDB = new File("Database.csv");
 
-    public static void main(String[] args) {
-        comprobarBD();
-        menuOpcionesPrincipal();
+
+
+    //String separator = FileSystems.getDefault().getSeparator();
+    //String filename = String.format("src%scom%slauracalderon%sArchivoDataBase.csv",separator,separator,separator,separator,separator,separator);
+
+    public void FileDoctor(){
+        String separator = FileSystems.getDefault().getSeparator();
+        String fileName = String.format("src%scom%slauracalderon%sDataBaseDoctor.csv",separator,separator,separator,separator,separator);
+        Path path = Paths.get(fileName);
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        if(!Files.exists(path)){
+            File file = new File(String.valueOf(path));
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error"+ e);
+            }
+        }
+
+    }
+    public void FilePaciente(){
+        String separator = FileSystems.getDefault().getSeparator();
+        String fileName = String.format("src%scom%slauracalderon%sDataBasePaciente.csv",separator,separator,separator,separator,separator);
+        Path path = Paths.get(fileName);
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        if(!Files.exists(path)){
+            File file = new File(String.valueOf(path));
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error"+ e);
+            }
+        }
+
     }
 
-    private static void menuOpcionesPrincipal() {
+
+    public static void main(String[] args) {
+        /*comprobarBD();
+        menuOpcionesPrincipal();*/
+
+    }
+
+    public static void menuOpcionesPrincipal() {
         Scanner opmenu = new Scanner(System.in);
+        //File ArchivoDB = new File("ArchivoDatabase.csv");
+
         int opcion;
 
         do {
@@ -34,18 +76,20 @@ public class AgendaCitas {
             System.out.println("---------------------------------------------------------------------\n");
 
             System.out.println("Ingresar una de las opciones siguientes:\n");
-            System.out.println("1.Agregar una nueva cita");
-            System.out.println("2.Buscar una cita");
-            System.out.println("3.Actualizar cita");
-            System.out.println("4.Eliminar cita");
-            System.out.println("5.Mostrar todas las citas agendadas");
-            System.out.println("6.Salir del programa");
+            System.out.println("1.Agregar doctor y especialidad: ");
+            System.out.println("2.Agregar pacientes: ");
+            System.out.println("3.Agregar una nueva cita");
+            System.out.println("4.Buscar una cita");
+            System.out.println("5.Actualizar cita");
+            System.out.println("6.Eliminar cita");
+            System.out.println("7.Mostrar todas las citas agendadas");
+            System.out.println("8.Salir del programa");
             opcion = opmenu.nextInt();
             System.out.println("---------------------------------------------------------------------\n");
             switch (opcion) {
                 case 1:
                     System.out.println("-----------------------------------------------------------------------");
-                    System.out.println("                 AGREGAR NUEVO REGISTRO                                ");
+                    System.out.println("                 AGREGAR NUEVO DOCTOR                                ");
                     System.out.println("---------------------------------------------------------------------\n");
                     agregarRegistro();
                     System.out.println("---------------------------------------------------------------------\n");
@@ -74,7 +118,7 @@ public class AgendaCitas {
                     System.out.println("                 Fin de Archivos");
                     System.out.println("----------------------------------------------------------------------\n");
                     break;
-                case 6:
+                case 8:
                     System.out.println("Salir");
                     break;
                 default:
@@ -90,17 +134,22 @@ public class AgendaCitas {
     private static void menuBuscarRegistro() {
     }
 
-    private static void agregarRegistro() {
-        Scanner agregar = new Scanner(System.in);
-        String sperador = "  |  ";
-        int tamañao = 0; //para ver el tamaño del registro
+    private static void agregarRegistro()  {
+        AgendaCitas nose = new AgendaCitas();
+        nose.FileDoctor();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa nombre del doctor ");
+        var nombre = entrada.next();
+        System.out.println("Ingresar ID: ");
+        var numero = entrada.next();
 
 
     }
 
     private static void comprobarBD () {
-        try{
-            if(ArchivoDB.exists()){
+        /*try{
+
+            if(DataBasePaciente.exists()){
                 System.out.println("Puede trabajar en el archivo. \n");
             }else { ArchivoDB.createNewFile();
                 System.out.println("Se ha creado el archivo, ya puedes trabajar. \n");
@@ -108,7 +157,8 @@ public class AgendaCitas {
         }catch(Exception e){
             System.out.println("Error"+e.getMessage());
             System.out.println("No se puede trabajar en el archivo. \n ");
-        }
+        }*/
         }
 
     }
+
