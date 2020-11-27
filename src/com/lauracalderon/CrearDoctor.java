d6c2ff441d1daa5b4e0273bed8a89b802647a33e
Valeriagd <valeriagd2001@gmail.com>
2020-11-27 17:12:02 -0600

    Modificacion a clase crear doctor

--- src/com/lauracalderon/CrearDoctor.java
+++ src/com/lauracalderon/CrearDoctor.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import com.csvreader.CsvReader;
 
 
@@ -14,9 +16,33 @@ public class CrearDoctor {
 
    public void ListaDoctores()  {
         List<Doctor> Doctores = new ArrayList<Doctor>();
-        Doctores.add(new Doctor("Laura Calderon", "123456788", "laura25@gmail.com","Medico general","98765" ));
-        Doctores.add(new Doctor("Valeria Diaz","7654323456", "valeria24@gmail.com","Pediatra","65432345"));
-        Doctores.add(new Doctor("Melissa Luna", "1234565432", "melissa21@gmail.com", "Medico general", "54378"));
+
+
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("Ingresa el nombre del dotor");
+       var nombre = entrada.next();
+
+       Scanner leer = new Scanner(System.in);
+       System.out.println("Ingresa el telefono");
+       var telefono = leer.next();
+
+       Scanner entrada1 = new Scanner(System.in);
+       System.out.println("Ingresa el correo");
+       var correo = entrada1.next();
+
+       Scanner entrada2 = new Scanner(System.in);
+       System.out.println("Ingresa la especialidad");
+       var especialidad = entrada2.next();
+
+       Scanner entrada3 = new Scanner(System.in);
+       System.out.println("Ingresa la cedula");
+       var cedula = entrada3.next();
+
+
+       Doctores.add(new Doctor(nombre, telefono, correo,especialidad,cedula ));
+
+
+
         //Doctores.add(new Doctor())
         ImportarCSV();
         ExportarCSV(Doctores);
@@ -70,6 +96,8 @@ public class CrearDoctor {
             CsvReader leerUsuarios = new CsvReader("DataBaseDoctor.csv");
             leerUsuarios.readHeaders();
 
+
+
             // Mientras haya lineas obtenemos los datos del archivo
             while(leerUsuarios.readRecord()) {
                 String Nombre = leerUsuarios.get(0);
@@ -78,20 +106,25 @@ public class CrearDoctor {
                 String Especialidad  = leerUsuarios.get(3);
                 String Cedula = leerUsuarios.get(4);
 
+
                 usuarios.add(new Doctor(Nombre, Telefono, Correo, Especialidad, Cedula)); // Añade la informacion a la lista
+
+
             }
 
-            leerUsuarios.close(); // Cierra el archivo
+            //leerUsuarios.close(); // Cierra el archivo
+
+
 
             // Recorremos la lista y la mostramos en la pantalla
-            for(Doctor user : usuarios) {
+            /*for(Doctor user : usuarios) {
                 System.out.println(user.getNOMBRE() + " , "
                         + user.getTELEFONO() + " , "
                         +user.getCORREO() + " , "
                         +user.getESPECIALIDAD() + " , "
                         +user.getCEDULA());
 
-            }
+            }*/
 
         } catch(FileNotFoundException e) {
             e.printStackTrace();
